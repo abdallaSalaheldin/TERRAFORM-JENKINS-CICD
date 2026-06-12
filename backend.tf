c894af922e0915dc03b49db269acc6a0834f7dec
abdallaSalaheldin <136724372+abdallaSalaheldin@users.noreply.github.com>
2026-06-12 15:51:33 +0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "jenkins-cicd-s3-demo"
+    bucket         = "jenkins-cicd-pipeline-production"
     key            = "my-terraform-environment/main"
     region         = "ap-south-1"
     dynamodb_table = "mrcloudbook-dynamo-db-table"
